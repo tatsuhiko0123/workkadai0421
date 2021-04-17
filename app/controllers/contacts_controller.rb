1da768004f872aa7f5ba1b0ed7be554fffd45c3a
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-17 16:54:41 +0900

    ペアワーク0417

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,19 @@
+class ContactsController < ApplicationController
+  def index
+  end
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path
+    else
+      render :new
+    end
+  end
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+end
